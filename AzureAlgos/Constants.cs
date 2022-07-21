052cdaf9fa0187aa28d4844fba5a25d07b201b3a
Robyn Stanco <robyn.stanco@gmail.com>
2022-07-20 23:21:54 -0400

    quick sort implementation

--- AzureAlgos/Constants.cs
+++ AzureAlgos/Constants.cs
@@ -6,6 +6,8 @@
     public static class Constants
     {
         public static string Constant = "O(1)";
+        public static string LogN = "O(log(n))";
+        public static string NLogN = "O(n*log(n))";
         public static string NSquared = "O(n^2)";
     }
 }
